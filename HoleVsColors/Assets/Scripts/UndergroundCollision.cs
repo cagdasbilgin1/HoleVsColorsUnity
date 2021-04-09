e4d8bdcffd0448d436424daba76534f04359a506
çağdaş bilgin <cagdasbilgin1@gmail.com>
2021-04-09 21:14:50 +0300

    feat: add progressbar for level

---
+++ HoleVsColors/Assets/Scripts/UndergroundCollision.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class UndergroundCollision : MonoBehaviour
+{
+    private void OnTriggerExit(Collider other)
+    {
+        if (other.tag.Equals("Object"))
+        {
+            Debug.Log("object");
+        }
+        if (other.tag.Equals("Obstacle"))
+        {
+            Debug.Log("obstacle");
+
+        }
+    }
+}
