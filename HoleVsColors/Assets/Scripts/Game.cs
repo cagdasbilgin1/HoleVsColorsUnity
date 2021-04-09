6f4aa4800cea5bf6fb0bbcfa587505ddce31753e
çağdaş bilgin <cagdasbilgin1@gmail.com>
2021-04-09 15:15:46 +0300

    feat: add hole movement

---
+++ HoleVsColors/Assets/Scripts/Game.cs
@@ -0,0 +1,9 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Game
+{
+    public static bool isGameOver = false;
+    public static bool isMoving = false;
+}
