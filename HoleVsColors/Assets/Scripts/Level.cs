4e8271ba53f56d2f90da77a0f058ebb8da994ae9
çağdaş bilgin <cagdasbilgin1@gmail.com>
2021-04-13 01:34:25 +0300

    level design

--- HoleVsColors/Assets/Scripts/Level.cs
+++ HoleVsColors/Assets/Scripts/Level.cs
@@ -2,6 +2,7 @@
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.SceneManagement;
+using UnityEngine.UI;
 
 public class Level : MonoBehaviour
 {
@@ -27,9 +28,37 @@ public class Level : MonoBehaviour
 
     [SerializeField] Transform objectsParent;
 
+    [Space]
+    [Header("Level Objects & Obstacles")]
+    [SerializeField] Material groundMaterial;
+    [SerializeField] Material objectMaterial;
+    [SerializeField] Material obstacleMaterial;
+    [SerializeField] SpriteRenderer groundBorderSprite;
+    [SerializeField] SpriteRenderer groundSideSprite;
+    [SerializeField] Image progressFillImage;
+
+    [SerializeField] SpriteRenderer bgFadeSprite;
+
+    [Space]
+    [Header("Level Colors")]
+    [Header("Ground")]
+    [SerializeField] Color groundColor;
+    [SerializeField] Color bordersColor;
+    [SerializeField] Color sideColor;
+    [Header("Objects & Obstacles")]
+    [SerializeField] Color objectColor;
+    [SerializeField] Color obstacleColor;
+    [Header("UI (progress)")]
+    [SerializeField] Color progressFillColor;
+
+    [Header("Background")]
+    [SerializeField] Color cameraColor;
+    [SerializeField] Color fadeColor;
+
     void Start()
     {
         CountObjects();
+        UpdateLevelColors();
     }
 
     // Update is called once per frame
@@ -51,5 +80,26 @@ public class Level : MonoBehaviour
     public void RestartLevel()
     {
         SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex);
+    }
+
+    void UpdateLevelColors()
+    {
+        groundMaterial.color = groundColor;
+        groundSideSprite.color = sideColor;
+        groundBorderSprite.color = bordersColor;
+
+        obstacleMaterial.color = obstacleColor;
+        objectMaterial.color = objectColor;
+
+        progressFillImage.color = progressFillColor;
+
+        Camera.main.backgroundColor = cameraColor;
+        bgFadeSprite.color = fadeColor;
+
+    }
+
+    private void OnValidate()
+    {
+        UpdateLevelColors();
     }
 }
